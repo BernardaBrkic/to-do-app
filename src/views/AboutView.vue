03a49b8dad1d9f701d2b9a94ed04495f558e21cc
Bernarda Brkić <bernarda.brkic1@gmail.com>
2022-03-19 13:26:06 +0100

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,57 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <BackgroundImage />
+    <div class="content">
+      <div class="wrapper">
+        <section>
+          <h1>About To-Do App</h1>
+          <p>
+            The app allows you to add, delete and edit tasks you have to do in
+            your everyday life.
+          </p>
+        </section>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+import BackgroundImage from "../components/BackgroundImage.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    BackgroundImage,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  width: 100%;
+  max-width: 80%;
+  margin: 0 auto;
+}
+.content {
+  height: 100vh;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  z-index: 2;
+  display: flex;
+  align-items: center;
+}
+.content .wrapper {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  flex: 50%;
+}
+section {
+  display: flex;
+  flex-direction: column;
+  width: 35%;
+  color: #efefef;
+  text-align: right;
+}
+</style>
