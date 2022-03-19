9863ca0be33ca2410b1a6d0425e2e81ede209a49
Bernarda Brkić <bernarda.brkic1@gmail.com>
2022-03-19 13:25:55 +0100

    Added background image as a vue component.

---
+++ src/components/BackgroundImage.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="background">
+    <div class="overlay"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BackgroundImage",
+};
+</script>
+
+<style lang="scss" scoped>
+.background {
+  background-image: url("../assets/background.png");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: -10px 60%;
+  background-color: #445c62;
+  background-attachment: fixed;
+  height: 100vh;
+  width: 100%;
+  z-index: -1;
+}
+.overlay {
+  position: fixed;
+  height: 100vh;
+  width: 100%;
+  z-index: 2;
+  top: 0;
+  background-image: linear-gradient(to right, #00000000, #000000c5);
+}
+</style>
