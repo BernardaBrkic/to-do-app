6b04988c1bc1a2cbc94a3226f85af4a70426c8c5
Bernarda Brkić <bernarda.brkic1@gmail.com>
2022-03-19 13:27:13 +0100

    Added task list page.

---
+++ src/views/TaskList.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="task-list">
+    <div class="overlay"></div>
+    <div class="content">
+      <div class="wrapper">
+        <section class="empty-list">
+          <h1>Task list</h1>
+          <p>You have no tasks added onto the task list. &#128532;</p>
+          <p>
+            Go visit <router-link to="/addtask">Add Task</router-link> and list
+            all your tasks. &#128578;
+          </p>
+        </section>
+        <section class="list">
+          <h1>Edit tasks</h1>
+          <ul>
+            <li></li>
+          </ul>
+        </section>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// import { content } from "./AddToDo.vue";
+
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.task-list {
+  background-color: #445c62;
+  height: 100vh;
+  background-image: url("../assets/task-list.png");
+  background-size: 100vh;
+  background-repeat: no-repeat;
+}
+
+p a {
+  text-decoration: none;
+  text-transform: uppercase;
+  font-weight: bold;
+  color: #4cb494;
+}
+// .empty-list {
+//   display: none;
+// }
+.list {
+  display: none;
+}
+</style>
