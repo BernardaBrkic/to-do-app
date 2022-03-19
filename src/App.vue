c5072d4c49149f54173a617b78d95f8effbd5205
Bernarda Brkić <bernarda.brkic1@gmail.com>
2022-03-19 13:23:29 +0100

    Added navbar links.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,91 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <div class="wrapper">
+      <router-link to="/contactus">Contact us</router-link>
+      <router-link to="/about">About</router-link>
+      <router-link to="/tasklist">Task list</router-link>
+      <router-link to="/addtask">Add Task</router-link>
+      <router-link to="/">Home</router-link>
+    </div>
   </nav>
   <router-view />
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import url("https://fonts.googleapis.com/css2?family=Nunito");
 
+* {
+  font-family: "Nunito", sans-serif;
+}
+body {
+  padding: 0;
+  margin: 0;
+}
+.wrapper {
+  width: 100%;
+  max-width: 80%;
+  margin: 0 auto;
+}
 nav {
-  padding: 30px;
+  position: fixed;
+  width: 100%;
+  z-index: 3;
+  top: 0;
 }
-
 nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: #efefef;
+  text-decoration: none;
+  text-transform: uppercase;
+  float: right;
+  margin: 20px 2%;
+  font-size: 18px;
+  text-shadow: 2px 2px 3px #000;
 }
-
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: #f3b58d;
+  transition: 0.2s;
+}
+section h1 {
+  color: #4cb494;
+  text-shadow: 2px 2px 3px #000;
+  font-size: 35px;
+}
+section p {
+  font-size: 18px;
+}
+.wrapper {
+  width: 100%;
+  max-width: 80%;
+  margin: 0 auto;
+}
+.overlay {
+  position: fixed;
+  height: 100vh;
+  width: 100%;
+  top: 0;
+  background-image: linear-gradient(to right, #00000000, #000000c5);
+}
+.content {
+  height: 100vh;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  z-index: 2;
+  display: flex;
+  align-items: center;
+}
+.content .wrapper {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  flex: 50%;
+}
+section {
+  display: flex;
+  flex-direction: column;
+  width: 35%;
+  color: #efefef;
+  text-align: right;
 }
 </style>
