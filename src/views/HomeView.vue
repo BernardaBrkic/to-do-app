a99244b986dbb5d656953ae53aac8a17ca41e9be
Bernarda Brkić <bernarda.brkic1@gmail.com>
2022-04-14 22:57:22 +0200

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <BackgroundImage />
+    <div class="overlay"></div>
     <div class="content">
       <div class="wrapper">
         <section>
@@ -9,25 +9,30 @@
             It's hard to keep everything you have to do under control. Keep
             track of all your tasks in one place.
           </p>
+          <p>
+            The app allows you to add, delete and edit tasks you have to do in
+            your everyday life.
+          </p>
         </section>
       </div>
     </div>
   </div>
 </template>
 
-<script>
-import BackgroundImage from "../components/BackgroundImage.vue";
-
-export default {
-  name: "HomeView",
-  components: {
-    BackgroundImage,
-  },
-};
-</script>
+<script></script>
 
 <style lang="scss" scoped>
 section h1 {
   text-transform: uppercase;
+}
+.home {
+  background-image: url("../assets/background.png");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: -10px 60%;
+  background-color: #445c62;
+  background-attachment: fixed;
+  height: 100vh;
+  z-index: -1;
 }
 </style>
